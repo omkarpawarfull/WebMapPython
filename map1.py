3bf028ceb1782606119677953ddc21be91c126db
omkar pawar <omkarpawarfull@gmail.com>
2017-07-11 20:00:33 -0500

    2
    Reading a txt file about volcanoes and adding marker with the hieght info.

--- map1.py
+++ map1.py
@@ -1,7 +1,14 @@
 import folium
+import pandas
+
+data=pandas.read_csv("Volcanoes.txt")
+lat=list(data["LAT"])
+lon=list(data["LON"])
+elev=list(data["ELEV"])
+
 map=folium.Map(location=[38.58,-99.09],zoom_start=6,tiles="Mapbox Bright")
 fg=folium.FeatureGroup(name="My Map")
-for cordinate in [[38.2,-99.1],[38.2,-99.9]]:
-    fg.add_child(folium.Marker(location=cordinate,popup="Hi I am a Marker",icon=folium.Icon(color="green")))
+for i,j,k in zip(lat,lon,elev):
+    fg.add_child(folium.Marker(location=[i,j],popup=str(k)+" m",icon=folium.Icon(color="green")))
 map.add_child(fg)
 map.save("Map1.html")
