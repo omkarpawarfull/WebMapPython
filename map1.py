0b16c27a7dd599945f434f4594f383ef0e6c17bb
omkar pawar <omkarpawarfull@gmail.com>
2017-07-11 20:16:55 -0500

    3
    Changed the icons to circle popup

--- map1.py
+++ map1.py
@@ -8,7 +8,16 @@ elev=list(data["ELEV"])
 
 map=folium.Map(location=[38.58,-99.09],zoom_start=6,tiles="Mapbox Bright")
 fg=folium.FeatureGroup(name="My Map")
+
+def color_producer(elevation):
+    if elevation < 1000:
+        return "green"
+    elif 1000<=elevation<3000:
+        return "orange"
+    else:
+        return "red"
+
 for i,j,k in zip(lat,lon,elev):
-    fg.add_child(folium.Marker(location=[i,j],popup=str(k)+" m",icon=folium.Icon(color="green")))
+    fg.add_child(folium.CircleMarker(location=[i,j],popup=str(k)+" m",radius=4,color=color_producer(k),fill_color=color_producer(k)))
 map.add_child(fg)
 map.save("Map1.html")
